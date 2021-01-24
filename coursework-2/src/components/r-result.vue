5bd1c954edb4d5015af1ab3426ba866bdf552065
scripdoc <alesis.x@mail.ru>
2021-01-24 10:42:35 +0300

    feat: add coursework-2 solution

---
+++ coursework-2/src/components/r-result.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="card card-w70">
+    <h3 v-if="!result.length">Добавьте первый блок, чтобы увидеть результат</h3>
+
+    <div
+      v-else
+      v-for="(item, idx) in result"
+      v-html="getTemplate(item)"
+      :key="idx + item.blockType"
+      :class="['block', item.blockType]"
+    ></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'RResult',
+  props: {
+    result: { type: Array, default: () => [] }
+  },
+  methods: {
+    getTemplate({ blockType, content }) {
+      switch (blockType) {
+        case 'title':
+          return `<h1>${content}</h1>`
+        case 'subtitle':
+          return `<h2>${content}</h2>`
+        case 'avatar':
+          return `<img src="${content}" />`
+        case 'text':
+          return `<p>${content}</p>`
+        default:
+          return `<h1>Такого блока не существует</h1>`
+      }
+    }
+  }
+}
+</script>
